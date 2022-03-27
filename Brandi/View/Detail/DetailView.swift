2bf5b209a5667abc6df9d459493f468bf1d5fd90
makemakeway <makemakeway@gmail.com>
2022-03-28 02:32:58 +0900

    Add: Detail View SwiftUI로 작성, iOS 14 이상 대응 완료

--- Brandi/View/Detail/DetailView.swift
+++ Brandi/View/Detail/DetailView.swift
@@ -7,14 +7,56 @@
 
 import SwiftUI
 
+import Kingfisher
+
 struct DetailView: View {
+    @Environment(\.presentationMode) var mode
+    let document: Document
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            Color.black
+                .edgesIgnoringSafeArea(.vertical)
+            GeometryReader { reader in
+                VStack(alignment: .leading) {
+                    Button {
+                        mode.wrappedValue.dismiss()
+                    } label: {
+                        Image(systemName: "xmark")
+                            .foregroundColor(.white)
+                    }
+                    .padding()
+                    
+                    ScrollView(.vertical, showsIndicators: false) {
+                        VStack {
+                            if #available(iOS 14.0, *) {
+                                KFImage(URL(string: document.imageURL))
+                                    .placeholder({
+                                        Image("placeholder")
+                                            .resizable()
+                                            .aspectRatio(contentMode: .fit)
+                                    })
+                                    .resizable()
+                                    .aspectRatio(contentMode: .fit)
+                                
+                            } else {
+                                
+                            }
+                            
+                            Text(document.displaySitename)
+                            Text(document.datetime)
+                                .padding(.bottom)
+                        }
+                        .foregroundColor(.white)
+                    }
+                }
+            }
+        }
     }
 }
 
 struct DetailView_Previews: PreviewProvider {
     static var previews: some View {
-        DetailView()
+        DetailView(document: Document(collection: "", thumbnailURL: "", imageURL: "", width: 1, height: 1, displaySitename: "", docURL: "", datetime: ""))
     }
 }
