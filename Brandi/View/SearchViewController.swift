6c0720360496489d2659bd7044852bd69f311b83
makemakeway <makemakeway@gmail.com>
2022-03-28 00:11:06 +0900

    Add: 이미지 검색 결과 페칭 및 페이지네이션 구현

--- Brandi/View/SearchViewController.swift
+++ Brandi/View/SearchViewController.swift
@@ -7,25 +7,89 @@
 
 import UIKit
 
+import RxCocoa
+import RxSwift
+import Toast
+
 final class SearchViewController: UIViewController {
+    
     //MARK: Properties
-    
-    
+    private let viewModel: SearchViewModel = SearchViewModel()
+    private var disposeBag: DisposeBag = DisposeBag()
+    private var dataSource: UICollectionViewDiffableDataSource<Section, Document>!
     
     //MARK: UI
     let mainView = SearchView()
     
-    
     //MARK: Method
     
-    func searchControllerConfig() {
-        let searchController = UISearchController(searchResultsController: nil)
-        self.navigationItem.searchController = searchController
-        self.navigationItem.searchController?.searchBar.placeholder = "검색"
+    private func bind() {
+        //MARK: Input binding
+        mainView.searchBar.rx.text.orEmpty
+            .bind(to: viewModel.input.searchText)
+            .disposed(by: disposeBag)
+        
+        mainView.collectionView.rx.willDisplayCell
+            .bind(with: self) { owner, args in
+                let indexPath = args.at
+                owner.fetchNextPage(indexPath: indexPath.item)
+                print(indexPath.item)
+            }
+            .disposed(by: disposeBag)
+        
+        //MARK: Output binding
+        viewModel.output.errorMessage
+            .bind(with: self) { owner, message in
+                owner.mainView.makeToast(message, duration: 2.0, position: .center)
+            }
+            .disposed(by: disposeBag)
+        
+        viewModel.output.searchData
+            .bind(with: self) { owner, data in
+                var snapShot = NSDiffableDataSourceSnapshot<Section, Document>()
+                snapShot.appendSections([.first])
+                snapShot.appendItems(data.documents, toSection: .first)
+                owner.dataSource.apply(snapShot, animatingDifferences: true)
+            }
+            .disposed(by: disposeBag)
+        
+        viewModel.output.appendData
+            .bind(with: self) { owner, data in
+                var snapShot = owner.dataSource.snapshot()
+                snapShot.appendItems(data.documents)
+                owner.dataSource.apply(snapShot)
+            }
+            .disposed(by: disposeBag)
+    }
+    
+    private func fetchNextPage(indexPath: Int) {
+        let currentPage = viewModel.input.pageNumber.value
+        
+        if (currentPage * Constants.Page.size) - 1 == indexPath {
+            viewModel.input.pageNumber.accept(currentPage + 1)
+        }
+    }
+    
+    func dataSourceConfig() {
+        self.dataSource = UICollectionViewDiffableDataSource<Section, Document>(collectionView: mainView.collectionView, cellProvider: { collectionView, indexPath, itemIdentifier in
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: SearchCollectionViewCell.reuseIdentifier, for: indexPath) as? SearchCollectionViewCell else { return UICollectionViewCell() }
+            
+            cell.configure(url: itemIdentifier.thumbnailURL)
+            
+            return cell
+        })
     }
     
     
     //MARK: LifeCycle
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nil, bundle: nil)
+        dataSourceConfig()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
     
     override func loadView() {
         self.view = mainView
@@ -33,6 +97,7 @@ final class SearchViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        searchControllerConfig()
+        self.navigationItem.titleView = mainView.searchBar
+        bind()
     }
 }
--- Brandi/ViewModel/SearchViewModel.swift
+++ Brandi/ViewModel/SearchViewModel.swift
@@ -6,3 +6,65 @@
 //
 
 import Foundation
+
+import RxCocoa
+import RxSwift
+
+final class SearchViewModel: ViewModelType {
+    
+    struct Input {
+        let searchText: BehaviorRelay<String> = BehaviorRelay(value: "")
+        let pageNumber: BehaviorRelay<Int> = BehaviorRelay(value: 1)
+        let endOfPage: BehaviorRelay<Bool> = BehaviorRelay(value: false)
+    }
+    
+    struct Output {
+        let searchData: PublishRelay<SearchData> = PublishRelay<SearchData>()
+        let errorMessage: PublishRelay<String> = PublishRelay<String>()
+        let appendData: PublishRelay<SearchData> = PublishRelay<SearchData>()
+    }
+    
+    var disposeBag: DisposeBag = DisposeBag()
+    
+    let input = Input()
+    let output = Output()
+    
+    func transform() {
+        input.searchText
+            .debounce(.seconds(1), scheduler: MainScheduler.instance)
+            .filter { !($0.isEmpty) }
+            .distinctUntilChanged()
+            .asObservable()
+            .flatMap { KakaoAPIService.shared.fetchSearchResponse(query: $0) }
+            .bind(with: self) { owner, result in
+                switch result {
+                case .success(let value):
+                    owner.input.pageNumber.accept(1)
+                    owner.output.searchData.accept(value)
+                case .failure(let error):
+                    owner.output.errorMessage.accept(error.rawValue)
+                }
+            }
+            .disposed(by: disposeBag)
+        
+        input.pageNumber
+            .filter { $0 != 1 }
+            .distinctUntilChanged()
+            .withUnretained(self)
+            .flatMap { owner, page in KakaoAPIService.shared.fetchSearchResponse(query: owner.input.searchText.value, page: page) }
+            .bind(with: self) { owner, result in
+                switch result {
+                case .success(let value):
+                    owner.output.appendData.accept(value)
+                case .failure(let error):
+                    owner.output.errorMessage.accept(error.rawValue)
+                }
+            }
+            .disposed(by: disposeBag)
+    }
+    
+    init() {
+        transform()
+    }
+    
+}
