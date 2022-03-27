f850b4375ddc31594d330c9eac4eeba2fe2c372f
makemakeway <makemakeway@gmail.com>
2022-03-28 04:20:38 +0900

    Add: 데이터 모델 작성

--- Brandi/Model/SearchData.swift
+++ Brandi/Model/SearchData.swift
@@ -6,3 +6,42 @@
 //
 
 import Foundation
+
+// MARK: - SearchData
+struct SearchData: Codable {
+    let meta: Meta
+    let documents: [Document]
+}
+
+// MARK: - Document
+struct Document: Codable, Hashable {
+    let collection: String
+    let thumbnailURL: String
+    let imageURL: String
+    let width, height: Int
+    let displaySitename: String
+    let docURL: String
+    let datetime: String
+
+    enum CodingKeys: String, CodingKey {
+        case collection
+        case thumbnailURL = "thumbnail_url"
+        case imageURL = "image_url"
+        case width, height
+        case displaySitename = "display_sitename"
+        case docURL = "doc_url"
+        case datetime
+    }
+}
+
+// MARK: - Meta
+struct Meta: Codable {
+    let totalCount, pageableCount: Int
+    let isEnd: Bool
+
+    enum CodingKeys: String, CodingKey {
+        case totalCount = "total_count"
+        case pageableCount = "pageable_count"
+        case isEnd = "is_end"
+    }
+}
