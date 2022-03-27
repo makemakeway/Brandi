4441b9e071ff207ebbf02a44474e061c442335fe
makemakeway <makemakeway@gmail.com>
2022-03-27 21:10:20 +0900

    Add: API Call 관리용 클래스 추가

--- Brandi/Network/KakaoAPIService.swift
+++ Brandi/Network/KakaoAPIService.swift
@@ -6,3 +6,80 @@
 //
 
 import Foundation
+
+import Alamofire
+import RxSwift
+
+final class KakaoAPIService {
+    
+    enum APIError: Error {
+        case badRequest
+        case unAuthorized
+        case forbidden
+        case tooManyRequest
+        case internalServerError
+        case badGateway
+        case serviceUnavailable
+        case networkConnection
+        case unknownError
+    }
+    
+    static let shared = KakaoAPIService()
+    
+    func apiErrorHandler(status: Int) -> APIError? {
+        switch status {
+        case 200:
+            return nil
+        case 400:
+            return .badRequest
+        case 401:
+            return .unAuthorized
+        case 403:
+            return .forbidden
+        case 429:
+            return .tooManyRequest
+        case 500:
+            return .internalServerError
+        case 502:
+            return .badGateway
+        case 503:
+            return .serviceUnavailable
+        default:
+            return .unknownError
+        }
+    }
+    
+    func fetchSearchResponse(query: String, page: Int = 1, size: Int = 30) -> Single<SearchData?> {
+        return Single.create { single in
+            if !(Connectivity.isConnectedToInternet) {
+                single(.failure(APIError.networkConnection))
+            }
+            
+            let params: [String:String] = [
+                "query":query,
+                "page":"\(page)",
+                "size":"\(size)"
+            ]
+            
+            let headers: HTTPHeaders = [
+                Constants.Network.header:APIKey.key
+            ]
+            
+            AF.request(Constants.Network.url,
+                       method: .get,
+                       parameters: params,
+                       headers: headers)
+                .validate().responseDecodable(of: SearchData.self) { [weak self]response in
+                    if let error = self?.apiErrorHandler(status: response.response?.statusCode ?? 0) {
+                        single(.failure(error))
+                    }
+                    single(.success(response.value))
+                }
+            return Disposables.create()
+        }
+    }
+    
+    private init() {
+        
+    }
+}
---
+++ Brandi/ViewModel/SearchViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  SearchViewModel.swift
+//  Brandi
+//
+//  Created by 박연배 on 2022/03/27.
+//
+
+import Foundation
